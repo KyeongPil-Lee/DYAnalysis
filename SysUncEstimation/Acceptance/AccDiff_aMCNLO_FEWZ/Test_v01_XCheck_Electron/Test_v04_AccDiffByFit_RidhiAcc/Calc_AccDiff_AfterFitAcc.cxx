864905aa98756880d040a968cdc1952784b5b10d
Kyoengpil Lee <kplee@cern.ch>
2017-07-31 15:22:40 +0900

    calc. acceptance uncertainty using Ridhi's acceptance

---
+++ SysUncEstimation/Acceptance/AccDiff_aMCNLO_FEWZ/Test_v01_XCheck_Electron/Test_v04_AccDiffByFit_RidhiAcc/Calc_AccDiff_AfterFitAcc.cxx
@@ -0,0 +1,18 @@
+#include "Calc_AccDiff_AfterFitAcc.h"
+
+void Calc_AccDiff_AfterFitAcc()
+{
+	if( gSystem->mkdir( "./Local" ) == 0 ) printf("Directory [Local] is created\n");
+
+	TFile *f_output = TFile::Open("")
+	// vector< TString > vec_Channel = {"Muon", "Electron"};
+	vector< TString > vec_Channel = {"Electron"};
+
+	for( const auto& Channel: vec_Channel )
+	{		
+		FittingTool *tool = new FittingTool( Channel );
+		tool->Fit_All();
+		tool->Calc_AccDiff();
+		tool->Save();
+	}
+}
