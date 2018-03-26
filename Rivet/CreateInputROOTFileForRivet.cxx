5c38fa6a7357e22234c13714d963e5e1d63853ef
Kyoengpil Lee <kplee@cern.ch>
2018-03-26 23:16:25 +0200

    recipes for rivet plugin

--- Rivet/CreateInputROOTFileForRivet.cxx
+++ Rivet/CreateInputROOTFileForRivet.cxx
@@ -1,5 +1,6 @@
 #include <Include/PlotTools.h>
 
+void AssignUncertainty_ElectronChannel( TH1D* h_DXSec, TH1D* h_RelUnc_Stat, TH1D* h_RelUnc_Syst, Double_t RelUnc_Lumi );
 void CreateInputROOTFileForRivet()
 {
 	TString ROOTFilePath = gSystem->Getenv("KP_ROOTFILE_PATH");
@@ -18,6 +19,24 @@ void CreateInputROOTFileForRivet()
 	TH1D* h_DXSec_ee = Get_Hist(fileName_ee, "h_DiffXSec");
 	TH1D* h_DXSecFiducial_ee = Get_Hist(fileName_ee, "h_DiffXSec_Fiducial");
 
+	//////////////////////////////////////////////////////////////
+	// -- update the uncertainty of electron channel results -- //
+	TH1D* h_RelUnc_Stat = Get_Hist(fileName_ee, "h_RelUnc_Stat");
+	TH1D* h_RelUnc_Syst = Get_Hist(fileName_ee, "h_RelUnc_Syst");
+	AssignUncertainty_ElectronChannel( h_DXSec_ee, h_RelUnc_Stat, h_RelUnc_Syst, 0.023 );
+
+	TH1D* h_RelUnc_Stat_Fiducial = Get_Hist(fileName_ee, "h_RelUnc_Stat_Fiducial");
+	TH1D* h_RelUnc_Syst_Fiducial = Get_Hist(fileName_ee, "h_RelUnc_Syst_Fiducial");
+	AssignUncertainty_ElectronChannel( h_DXSecFiducial_ee, h_RelUnc_Stat_Fiducial, h_RelUnc_Syst_Fiducial, 0.023 );
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	// -- multiply bin width to each bin value because root2yoda automatically divide bin content by bin value :( -- //
+	h_DXSec_ll = MultiplyEachBin_byBinWidth( h_DXSec_ll );
+	h_DXSec_mumu = MultiplyEachBin_byBinWidth( h_DXSec_mumu );
+	h_DXSecFiducial_mumu = MultiplyEachBin_byBinWidth( h_DXSecFiducial_mumu );
+	h_DXSec_ee = MultiplyEachBin_byBinWidth( h_DXSec_ee );
+	h_DXSecFiducial_ee = MultiplyEachBin_byBinWidth( h_DXSecFiducial_ee );
 
 	TString outputFileName = "ROOTFile_InputForRivet.root";
 	TFile *f_output = TFile::Open(outputFileName, "RECREATE");
@@ -41,4 +60,21 @@ void CreateInputROOTFileForRivet()
 
 	cout << endl;
 	cout << outputFileName << " is created" << endl;
+}
+
+void AssignUncertainty_ElectronChannel( TH1D* h_DXSec, TH1D* h_RelUnc_Stat, TH1D* h_RelUnc_Syst, Double_t RelUnc_Lumi )
+{
+	Int_t nBin = h_DXSec->GetNbinsX();
+	for(Int_t i=0; i<nBin; i++)
+	{
+		Int_t i_bin = i+1;
+		Double_t DXSec = h_DXSec->GetBinContent(i_bin);
+
+		Double_t RelUnc_Stat = h_RelUnc_Stat->GetBinContent(i_bin) / 100.0;
+		Double_t RelUnc_Syst = h_RelUnc_Syst->GetBinContent(i_bin) / 100.0;
+		Double_t RelUnc_Tot = sqrt( RelUnc_Stat*RelUnc_Stat + RelUnc_Syst*RelUnc_Syst + RelUnc_Lumi*RelUnc_Lumi );
+		Double_t AbsUnc_Tot = DXSec * RelUnc_Tot;
+
+		h_DXSec->SetBinError(i_bin, AbsUnc_Tot);
+	}
 }
