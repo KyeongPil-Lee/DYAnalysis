44510add7499e028724f428d92bdded9be330c2a
Kyoengpil Lee <kplee@cern.ch>
2018-08-06 23:29:49 +0200

    use option -l

--- Test/v03_MuonEta/script_RunAll.sh
+++ Test/v03_MuonEta/script_RunAll.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 
-root -b -q 'MuonPlots.cxx++("MC", 0)' >&MuonPlots_MC_0.log&
-root -b -q 'MuonPlots.cxx++("MuonPhys", 0)' >&MuonPlots_MuonPhys_0.log&
-root -b -q 'MuonPlots.cxx++("MC", 1)' >&MuonPlots_MC_1.log&
-root -b -q 'MuonPlots.cxx++("MuonPhys", 1)' >&MuonPlots_MC_1.log&
+root -l -b -q 'MuonPlots.cxx++("MC", 0)' >&MuonPlots_MC_0.log&
+root -l -b -q 'MuonPlots.cxx++("MuonPhys", 0)' >&MuonPlots_MuonPhys_0.log&
+root -l -b -q 'MuonPlots.cxx++("MC", 1)' >&MuonPlots_MC_1.log&
+root -l -b -q 'MuonPlots.cxx++("MuonPhys", 1)' >&MuonPlots_MC_1.log&
 
 echo "Submission is finished"
