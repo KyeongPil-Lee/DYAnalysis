7080cec051ba71a82635f01f222f1d6b98c36dab
Kyoengpil Lee <kplee@cern.ch>
2019-07-23 15:15:35 +0900

    fix typo

--- ElecChannel/Uncertainties/EfficiencySF/v04_ignoreEffType/ID/SmearedEffSF_tagChange.cxx
+++ ElecChannel/Uncertainties/EfficiencySF/v04_ignoreEffType/ID/SmearedEffSF_tagChange.cxx
@@ -1,7 +1,7 @@
 #include <ElecChannel/Uncertainties/EfficiencySF/v04_ignoreEffType/SmearedEffSF_perMassBin.h>
 void SmearedEffSF_tagChange()
 {
-  TSTring effType_ignore = "ID";
+  TString effType_ignore = "ID";
   SmearedEffSFTool* tool = new SmearedEffSFTool("tagChange", effType_ignore);
   tool->Produce();
 }
