b3edcf2eab987ebd16b70f0a47f21f5dc7083fea
kplee <kplee@cern.ch>
2019-07-23 13:58:05 +0900

    the other pT range

---
+++ ElecChannel/Uncertainties/EfficiencySF/v03_ignorePtBinUnc/Pt30to40/SmearedEffSF_bkgChange.cxx
@@ -0,0 +1,14 @@
+#include <ElecChannel/Uncertainties/EfficiencySF/v03_ignorePtBinUnc/SmearedEffSF_perMassBin.h>
+void SmearedEffSF_bkgChange()
+{
+  // -- pt bin index
+  // -- [10, 20]: 0
+  // -- [20, 30]: 1
+  // -- [30, 40]: 2
+  // -- [40, 50]: 3
+  // -- [50, 200]: 4
+
+  Int_t ptBinIndex_ignore = 2;
+  SmearedEffSFTool* tool = new SmearedEffSFTool("bkgChange", ptBinIndex_ignore);
+  tool->Produce();
+}
