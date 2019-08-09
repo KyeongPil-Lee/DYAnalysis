745c85b233854dde929c1651b6019198b0a0ee29
kplee <kplee@cern.ch>
2019-08-09 15:06:46 +0900

    test w/ absolute size of difference

---
+++ ElecChannel/Uncertainties/EfficiencySF/v11_tagChange_trig_data/v01_absDiff/SmearedEffSF_tagChange.cxx
@@ -0,0 +1,14 @@
+#include "SmearedEffSF_perMassBin.h"
+
+void SmearedEffSF_tagChange()
+{
+  TString analyzerPath = gSystem->Getenv("KP_ANALYZER_PATH");
+  TString baseDir = analyzerPath+"/ElecChannel/EfficiencySF/TnPResult";
+
+  TString fileName_reco = baseDir+"/ROOTFile_RecoSF_April2019.root";
+  TString fileName_ID   = baseDir+"/ROOTFile_IDSF_April2019.root";
+  TString fileName_trig = "../TnPResult/ROOTFile_trigSF_SMP17001.root";
+
+  SmearedEffSFTool* tool = new SmearedEffSFTool("tagChange", fileName_reco, fileName_ID, fileName_trig);
+  tool->Produce();
+}
