d192baee3c0f3bbaf97b6e9589ad86e51879c0cc
kplee <kplee@cern.ch>
2019-08-11 16:22:46 +0900

    redo

--- ElecChannel/Uncertainties/EfficiencySF/SmearedEff/bkgChange/4th/SmearedEffSF_givenType_indexRange.cxx
+++
@@ -1,8 +0,0 @@
-#include <ElecChannel/Uncertainties/EfficiencySF/SmearedEffSF_perMassBin.h>
-
-void SmearedEffSF_GivenType_IndexRange(TString uncType, Int_t i_start, Int_t i_end )
-{
-  SmearedEffSFTool* tool = new SmearedEffSFTool(uncType);
-  tool->Set_IndexRange(i_start, i_end);
-  tool->Produce();
-}
