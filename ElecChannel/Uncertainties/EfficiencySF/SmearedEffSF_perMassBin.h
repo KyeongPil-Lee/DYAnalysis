ced10d8f8a9648ffa1178b823a724a8494ae63bc
kplee <kplee@cern.ch>
2019-08-11 16:12:15 +0900

    add index range function

--- ElecChannel/Uncertainties/EfficiencySF/SmearedEffSF_perMassBin.h
+++ ElecChannel/Uncertainties/EfficiencySF/SmearedEffSF_perMassBin.h
@@ -1,12 +1,19 @@
 #include <ElecChannel/EfficiencySF/TnPEfficiency.h>
 
-#define nEffMap 2000
+// #define nEffMap 2000
 // #define nEffMap 10
 
 class SmearedEffSFTool
 {
 public:
   TString uncType_;
+
+  Int_t nEffMap_ = 2000;
+
+  // -- index used for smeared eff.SF histogram name
+  Bool_t setIndexRange_ = kFALSE;
+  Int_t index_start_ = -1;
+  Int_t index_end_ = -1;
 
   TH1D* h_mass_effPass_noSF_;
 
@@ -15,9 +22,9 @@ public:
   TH1D* h_mass_effPass_withSF_cv_;
   TH1D* h_effSF_perMassBin_cv_;
 
-  TnPEfficiency* tnpEff_smeared_[nEffMap];
-  TH1D* h_mass_effPass_withSF_smeared_[nEffMap];
-  TH1D* h_effSF_perMassBin_smeared_[nEffMap];
+  vector<TnPEfficiency*> vec_tnpEff_smeared_;
+  vector<TH1D*> vec_hist_mass_effPass_withSF_smeared_;
+  vector<TH1D*> vec_hist_effSF_perMassBin_smeared_;
 
   Bool_t doFlipFlop_ = kFALSE;
 
@@ -26,17 +33,17 @@ public:
   TString fileName_trig_ = "";
   Bool_t usePrivateTnP_ = kFALSE;
 
+  TString fileName_output_ = "";
+
   SmearedEffSFTool(TString uncType)
   {
     uncType_ = uncType;
-    Init();
   }
 
   SmearedEffSFTool(TString uncType, Bool_t doFlipFlop )
   {
     uncType_ = uncType;
     doFlipFlop_ = doFlipFlop;
-    Init();
   }
 
   SmearedEffSFTool(TString uncType, TString fileName_reco, TString fileName_ID, TString fileName_trig )
@@ -53,11 +60,32 @@ public:
     cout << "  reconstruction efficiency: " << fileName_reco_ << endl;
     cout << "  ID efficiency: "             << fileName_ID_   << endl;
     cout << "  Trigger efficiency: "        << fileName_trig_ << endl;
-    Init();
+  }
+
+  void Set_nEffMap(Double_t n)
+  {
+    nEffMap_ = n;
+    printf("[SmearedEffSFTool::Set_nEffMap] nEffMap = %d\n", nEffMap_);
+  }
+
+  void Set_IndexRange(Int_t i_start, Int_t i_end)
+  {
+    setIndexRange_ = kTRUE;
+    index_start_ = i_start;
+    index_end_   = i_end;
+    nEffMap_ = (i_end - i_start) + 1;
+
+    printf("[SmearedEffSFTool::Set_IndexRange] the range of the index: %d < index < %d (nEffMap = %d)\n", index_start_, index_end_, nEffMap_);
+  }
+
+  void Set_OutputFileName(TString name )
+  {
+    fileName_output_ = name;
   }
 
   void Produce()
   {
+    Init();
     ProduceMassHist();
     Save();
   }
@@ -73,20 +101,25 @@ private:
     h_mass_effPass_withSF_cv_ = DYTool::MakeHist_DXSecBin("h_mass_effPass_withSF_cv");
     h_effSF_perMassBin_cv_ = nullptr; // -- will be made at the end of ProduceMassHist
 
-    for(Int_t i=0; i<nEffMap; i++)
+    for(Int_t i=0; i<nEffMap_; i++)
     {
-      if( !usePrivateTnP_ ) tnpEff_smeared_[i] = new TnPEfficiency();
-      else                  tnpEff_smeared_[i] = new TnPEfficiency(fileName_reco_, fileName_ID_, fileName_trig_);
+      TnPEfficiency* tnpEff_smeared_ = nullptr;
+      if( !usePrivateTnP_ ) tnpEff_smeared_ = new TnPEfficiency();
+      else                  tnpEff_smeared_ = new TnPEfficiency(fileName_reco_, fileName_ID_, fileName_trig_);
 
-      if( doFlipFlop_ ) tnpEff_smeared_[i]->doFlipFlop_ = kTRUE;
-      tnpEff_smeared_[i]->SmearingEff_GivenUncType(uncType_);
+      if( doFlipFlop_ ) tnpEff_smeared_->doFlipFlop_ = kTRUE;
+      tnpEff_smeared_->SmearingEff_GivenUncType(uncType_);
+      vec_tnpEff_smeared_.push_back( tnpEff_smeared_ );
 
-      TString numbering = TString::Format("%d", i);
-      h_mass_effPass_withSF_smeared_[i] = DYTool::MakeHist_DXSecBin("h_mass_effPass_withSF_smeared_"+numbering);
-      h_effSF_perMassBin_smeared_[i] = nullptr;
+
+      TString numbering = "";
+      if( setIndexRange_ ) numbering = TString::Format("%d", index_start_ + i);
+      else                 numbering = TString::Format("%d", i);
+      TH1D* h_mass_effPass_withSF_smeared_ = DYTool::MakeHist_DXSecBin("h_mass_effPass_withSF_smeared_"+numbering);
+      vec_hist_mass_effPass_withSF_smeared_.push_back( h_mass_effPass_withSF_smeared_ );
     }
 
-    cout << "[SmearedEffSFTool::Init] Generation of " << nEffMap << " randomized efficiency maps: done" << endl;
+    cout << "[SmearedEffSFTool::Init] Generation of " << nEffMap_ << " randomized efficiency maps: done" << endl;
     cout << endl;
   }
 
@@ -94,7 +127,7 @@ private:
   {
     TString dataPath = gSystem->Getenv("KP_DATA_PATH");
     TFile *f_ntuple = TFile::Open(dataPath+"/DY_forEff_M10to3000.root");
-    Double_t pt1, pt2, eta1, eta2, diM, lumiWeight, genWeight;
+    Double_t pt1, pt2, eta1, eta2, diM, lumiWeight, genWeight, puWeight;
 
     TTree *ntuple;
     ntuple = (TTree*)f_ntuple->Get("tree");
@@ -106,6 +139,7 @@ private:
     ntuple->SetBranchAddress("ZMass",&diM);
     ntuple->SetBranchAddress("lumiWeights",&lumiWeight);
     ntuple->SetBranchAddress("genWeights",&genWeight);
+    ntuple->SetBranchAddress("PUWeights",&puWeight);
 
     Long64_t nEvent = ntuple->GetEntries();
     cout << "nEvent: " << nEvent << endl;
@@ -116,15 +150,15 @@ private:
 
       ntuple->GetEvent(i_ev);
 
-      h_mass_effPass_noSF_->Fill( diM, lumiWeight*genWeight );
+      h_mass_effPass_noSF_->Fill( diM, lumiWeight*genWeight*puWeight );
 
       Double_t eventSF_cv = tnpEff_cv_->EfficiencySF_EventWeight( pt1, eta1, pt2, eta2 );
-      h_mass_effPass_withSF_cv_->Fill( diM, lumiWeight*genWeight*eventSF_cv );
+      h_mass_effPass_withSF_cv_->Fill( diM, lumiWeight*genWeight*puWeight*eventSF_cv );
 
-      for(Int_t i=0; i<nEffMap; i++)
+      for(Int_t i=0; i<nEffMap_; i++)
       {
-        Double_t eventSF_smeared = tnpEff_smeared_[i]->EfficiencySF_EventWeight( pt1, eta1, pt2, eta2 );
-        h_mass_effPass_withSF_smeared_[i]->Fill( diM, lumiWeight*genWeight*eventSF_smeared );
+        Double_t eventSF_smeared = vec_tnpEff_smeared_[i]->EfficiencySF_EventWeight( pt1, eta1, pt2, eta2 );
+        vec_hist_mass_effPass_withSF_smeared_[i]->Fill( diM, lumiWeight*genWeight*puWeight*eventSF_smeared );
       }
     } // -- end of event iteration
 
@@ -132,13 +166,16 @@ private:
     h_effSF_perMassBin_cv_->SetName("h_effSF_perMassBin_cv");
     h_effSF_perMassBin_cv_->Divide( h_mass_effPass_withSF_cv_, h_mass_effPass_noSF_ );
 
-    for(Int_t i=0; i<nEffMap; i++)
+    for(Int_t i=0; i<nEffMap_; i++)
     {
-      h_effSF_perMassBin_smeared_[i] = (TH1D*)h_mass_effPass_noSF_->Clone();
+      TH1D* h_effSF_perMassBin_smeared = (TH1D*)h_mass_effPass_noSF_->Clone();
 
-      TString numbering = TString::Format("%d", i);
-      h_effSF_perMassBin_smeared_[i]->SetName("h_effSF_perMassBin_smeared_"+numbering);
-      h_effSF_perMassBin_smeared_[i]->Divide( h_mass_effPass_withSF_smeared_[i], h_mass_effPass_noSF_ );
+      TString numbering = "";
+      if( setIndexRange_ ) numbering = TString::Format("%d", index_start_ + i);
+      else                 numbering = TString::Format("%d", i);
+      h_effSF_perMassBin_smeared->SetName("h_effSF_perMassBin_smeared_"+numbering);
+      h_effSF_perMassBin_smeared->Divide( vec_hist_mass_effPass_withSF_smeared_[i], h_mass_effPass_noSF_ );
+      vec_hist_effSF_perMassBin_smeared_.push_back( h_effSF_perMassBin_smeared );
     }
 
     cout << "[SmearedEffSFTool::ProduceMassHist] Done" << endl;
@@ -146,15 +183,22 @@ private:
 
   void Save()
   {
-    TString fileName = "ROOTFile_SmearedEffSF_perMassBin_"+uncType_+".root";
+    TString fileName = "";
+    if( fileName_output_ == "" )
+    {
+      if(setIndexRange_) fileName = "ROOTFile_SmearedEffSF_perMassBin_"+uncType_+TString::Format("_%d_to_%d.root", index_start_, index_end_);
+      else               fileName = "ROOTFile_SmearedEffSF_perMassBin_"+uncType_+".root";
+    }
+    else fileName = fileName_output_;
+
     TFile *f_output = TFile::Open(fileName, "RECREATE");
     f_output->cd();
     h_mass_effPass_noSF_->Write();
     h_mass_effPass_withSF_cv_->Write();
     h_effSF_perMassBin_cv_->Write();
 
-    for(Int_t i=0; i<nEffMap; i++)
-      h_effSF_perMassBin_smeared_[i]->Write();
+    for(Int_t i=0; i<nEffMap_; i++)
+      vec_hist_effSF_perMassBin_smeared_[i]->Write();
 
     f_output->Close();
     cout << "Output file: " << fileName << endl;
@@ -165,5 +209,7 @@ private:
 void SmearedEffSF_perMassBin()
 {
   SmearedEffSFTool* tool = new SmearedEffSFTool("bkgChange");
+  tool->Set_IndexRange(500, 509);
+  tool->Set_OutputFileName( "ROOTFile_SmearedEffSF_perMassBin_TEST.root" );
   tool->Produce();
 }
