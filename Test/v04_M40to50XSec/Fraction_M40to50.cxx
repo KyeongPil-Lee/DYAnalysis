539d71ce586713572a6678df52f20be81d87d499
kplee <kplee@cern.ch>
2020-04-01 18:53:43 +0900

    check M40to50 x-sec

---
+++ Test/v04_M40to50XSec/Fraction_M40to50.cxx
@@ -0,0 +1,91 @@
+#include <TChain.h>
+#include <TFile.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TLorentzVector.h>
+#include <TStopwatch.h>
+#include <TTimeStamp.h>
+#include <TString.h>
+#include <TLegend.h>
+#include <THStack.h>
+#include <TPad.h>
+#include <TCanvas.h>
+#include <TColor.h>
+#include <TAttMarker.h>
+#include <TF1.h>
+#include <TStyle.h>
+#include <TROOT.h>
+#include <TApplication.h>
+#include <vector>
+#include <TMath.h>
+
+// -- Customized Analyzer for Drel-Yan Analysis -- //
+#include <Include/DYAnalyzer.h>
+static inline void loadBar(int x, int n, int r, int w);
+
+void Fraction_M40to50()
+{
+  TString ntupleDir = "/data7/Users/kplee/backup/data4/Users/kplee/DYntuple/76X/v20160304_76X_MINIAODv2_DYLL_M10to50_25ns";
+  TString tag = "DYMuMu_M10to50";
+  TString xSec = 18610.0/3.0;
+  TString sumWeight = 7506956.0;
+
+  DYAnalyzer *analyzer = new DYAnalyzer( "IsoMu20_OR_IsoTkMu20" );
+
+  TChain *chain = new TChain("recoTree/DYTree");
+  chain->Add(ntupleDir+"/ntuple_*.root");
+
+  NtupleHandle *ntuple = new NtupleHandle( chain );
+  ntuple->TurnOnBranches_GenLepton();
+  // ntuple->TurnOnBranches_HLT();
+  // ntuple->TurnOnBranches_Muon();
+
+  Double_t sumWeight_tot = 0;
+  Double_t sumWeight_M40to50 = 0;
+
+  Int_t nEvent = chain->GetEntries();
+  cout << "\t[Total Events: " << nEvent << "]" << endl;
+  for(Int_t i=0; i<nEvent; i++)
+  {
+    loadBar(i+1, nEvent, 100, 100);
+
+    ntuple->GetEvent(i);
+
+    Bool_t isMuMuEvent = kFALSE;
+    isMuMuEvent = analyzer->SeparateDYLLSample_isHardProcess(Tag[i_tup], ntuple);
+
+    if( isMuMuEvent )
+    {
+      Double_t genWeight = ntuple->GENEvt_weight < 0 ? GenWeight = -1 : GenWeight = 1;
+
+      vector<GenLepton> vec_genLepton;
+      Int_t nGenLepton = ntuple->gnpair;
+      for(Int_t i_gen=0; i_gen<nGenLepton; i_gen++)
+      {
+        GenLepton genLepton;
+        genLepton.FillFromNtuple(ntuple, i_gen);
+        if( genLepton.isMuon() && genlep.isHardProcess )
+          vec_genLepton.push_back( genLepton );
+      }
+
+      Int_t nGenLeptonHP = (Int_t)vec_genLepton.size();
+      if( nGenLeptonHP != 2 )
+      {
+        cout << "nGenLeptonHP = " << nGenLeptonHP << " != 2 ... something wrong!!" << endl;
+        return;
+      }
+
+      Double_t diMuM = (vec_genLepton[0].Momentum + vec_genLepton[1].Momentum).M();
+
+      sumWeight_tot += sumWeight;
+      if( 40.0 <= diMuM && diMuM <= 50.0 ) sumWeight_M40to50 += sumWeight;
+
+    }
+
+  } // -- event iteration
+
+  Double_t frac_M40to50 = sumWeight_M40to50 / sumWeight_tot;
+
+  printf("[sum of weight] (total, 40 < M < 50 GeV) = (%.1lf, %.1lf)\n", sumWeight_M40to50, sumWeight_tot);
+  printf("   ---> fraction = %lf\n", frac_M40to50);
+}
